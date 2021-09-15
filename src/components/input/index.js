1ffc251b0d96a3deb618099068e614cdcc01b8d8
weahforsage <sanjasapple@gmail.com>
2021-09-15 19:07:43 +0800

    fix: position

--- src/components/input/index.js
+++ src/components/input/index.js
@@ -8,7 +8,7 @@ const Input = ({labelStyle, hideLabel, label, className, errorMessage, ...props}
             </label>}
             <input
                 {...props}
-                className={`block w-full rounded-md text-base border border-gray-200 placeholder-gray-400 
+                className={`block pl-3 pr-3 w-full rounded-md text-base border border-gray-200 placeholder-gray-400 
                 focus:outline-none focus:text-gray-900 focus:placeholder-gray-500 hover:placeholder-fontPrimary 
                 focus:ring-1 focus:ring-primary focus:border-primary sm:text-sm hover:border-primary 
                 hover:bg-white transition ease-in duration-100 " ${className}`}
