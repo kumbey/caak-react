a5411c0c753d7cf9401da097b30f867a132fd5ea
weahforsage <sanjasapple@gmail.com>
2021-09-16 16:28:37 +0800

    fix: added css import, postcss

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,42 +1,27 @@
-import React from 'react';
+import React from "react";
 import Loader from "../loader";
 
-const Button = (props) => {
-    if (props.secondary || props.primary)
-        return <button
-            onClick={props.onClick}
-            type={props.type}
-            className={`
-            inline-flex items-center text-sm font-medium shadow-sm  ${props.secondary ? "fontPrimary bg-gray-200" : props.background || "bg-primary text-white"} hover:bg-gray-300 focus:outline-none 
-            ${!props.circular ? "px-3 py-2" : ""}
-            ${props.rounded ? "rounded" : ""} 
-            ${props.circular ? "rounded-full" : ""}
-            ${props.className}`}
-        >
-            {props.iconPosition === "left" ? props.icon : ""}
-            {props.circular ? props.icon : ""}
-            {props.loading ? <Loader/> : props.children}
-            {props.iconPosition === "right" ? props.icon : ""}
-        </button>
-    return (
-        <button
-            onClick={props.onClick}
-            type={props.type}
-            className={`
-            inline-flex items-center shadow-sm focus:outline-none 
-            ${props.background || "bg-primary"}
-            ${props.text || "text-white"}
-            ${!props.circular ? "px-3 py-2" : ""}
-            ${props.rounded ? "rounded" : ""}
-            ${props.circular ? "rounded-full" : ""}
-            ${props.className}`}
-        >
-            {props.iconPosition === "left" ? props.icon : ""}
-            {props.loading ? <Loader/> : props.children}
-
-            {props.iconPosition === "right" ? props.icon : ""}
-        </button>
-    );
+const Button = ({
+  skin,
+  round,
+  circular,
+  iconPosition,
+  icon,
+  loading,
+  ...props
+}) => {
+  return (
+    <button
+      {...props}
+      className={`button ${skin ? skin : ""} ${round ? "round" : ""} ${
+        circular ? "circular" : ""
+      } ${props.className}`}
+    >
+      {iconPosition === "left" ? icon : ""}
+      {circular ? icon : ""}
+      {loading ? <Loader /> : props.children}
+      {iconPosition === "right" ? icon : ""}
+    </button>
+  );
 };
-
 export default Button;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,24 +1,25 @@
-import UserInformation from "../pages/Register/UserInformation";
 import Confirmation from "../pages/Register/Confirmation";
 import Login from "../pages/Login";
+import Register from "../pages/Register";
+import UserInformation from "../pages/Register/UserInformation";
 
 const Routes = [
-    {
-        path: "/",
-        exact: true,
-        page: () => <h2>Home page</h2>,
-    },
-    {
-        path: "/profile/userconfig",
-        exact: true,
-        auth: true,
-        page: () => <h2>User Config page</h2>,
-    },
-    {
-        path: "/sanjaa",
-        exact: true,
-        page: () => <Confirmation email={"sanjasapple@gmail.com"}/>,
-    }
-]
+  {
+    path: "/",
+    exact: true,
+    page: () => <h2>Home page</h2>,
+  },
+  {
+    path: "/profile/userconfig",
+    exact: true,
+    auth: true,
+    page: () => <h2>User Config page</h2>,
+  },
+  {
+    path: "/sanjaa",
+    exact: true,
+    page: () => <Confirmation />,
+  },
+];
 
-export default Routes
+export default Routes;
