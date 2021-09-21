9a16bb9cb01cbf9c1311dc1e949b0ae8aa76fcf4
weahforsage <sanjasapple@gmail.com>
2021-09-21 10:01:25 +0800

    fixed: tailwind color config and names

--- src/components/input/Select.js
+++ src/components/input/Select.js
@@ -10,8 +10,8 @@ const Select = (props) => {
                 id={props.id}
                 name={props.name}
                 className={`mt-1 w-full pl-3 pr-10 py-3 block w-full rounded-md text-base border border-gray-200 placeholder-gray-400
-                    focus:outline-none focus:text-gray-900 focus:placeholder-gray-500 hover:placeholder-fontPrimary
-                    focus:ring-1 focus:ring-primary focus:border-primary sm:text-sm hover:border-primary
+                    focus:outline-none focus:text-gray-900 focus:placeholder-gray-500 hover:placeholder-caak-generalblack
+                    focus:ring-1 focus:ring-caak-primary focus:border-caak-primary sm:text-sm hover:border-caak-primary
                     hover:bg-white transition ease-in duration-100 ${props.className}`}
                 defaultValue={props.defaultValue}
             >
