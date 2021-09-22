ffaa076227e3c47593132d43cef5df0c16432da0
weahforsage <sanjasapple@gmail.com>
2021-09-23 01:53:14 +0800

    input error message

--- src/components/input/index.js
+++ src/components/input/index.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 
 const Input = ({
   labelStyle,
@@ -6,27 +7,37 @@ const Input = ({
   className,
   errorMessage,
   id,
-  containerStyle,
+  type,
   ...props
 }) => {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
-    <div className={`input ${containerStyle && containerStyle}`}>
+    <div className={"input relative"}>
       {hideLabel ? null : (
         <label htmlFor={id} className={`${labelStyle}`}>
           {label}
         </label>
       )}
       <input
+        type={showPassword ? "text" : type}
         {...props}
         className={`${className} ${
           errorMessage ? `border border-caak-red` : ``
         }`}
       />
-      {errorMessage && (
+      {type === "password" ? (
+        <span
+          onClick={() => setShowPassword(!showPassword)}
+          className="icon-fi-rs-view right-c6 text-caak-darkBlue absolute cursor-pointer"
+          style={{ top: "14px" }}
+        />
+      ) : null}
+      {/*{errorMessage && (*/}
         <p className="error" id="email-error">
           {errorMessage}
         </p>
-      )}
+      {/*)}*/}
     </div>
   );
 };
