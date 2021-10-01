ef99892632bff6170cdb50e8363755fccbd72cbf
purewee790 <purwee790@gmail.com>
2021-10-01 15:13:55 +0800

    GroupSettings Response

--- src/pages/Group/Infromation.js
+++ src/pages/Group/Infromation.js
@@ -2,11 +2,13 @@ import {group_data} from './groupData'
 import { useState } from 'react';
 import Settings from './Settings';
 import DropTest from '../../components/PendingPost/DropTest';
+import { useHistory } from 'react-router';
 
 export default function Infromation() {
 
     const [isGroupEdit, setIsGroupEdit] = useState(false);
 
+    const history = useHistory();
 
     const toggleGroupEdit = () => {
         setIsGroupEdit(!isGroupEdit);
@@ -19,14 +21,14 @@ export default function Infromation() {
     };
 
     return (
-        <div style={{marginTop: "36px", marginInline: "500px"}} className="grid">
+        <div style={{marginTop: "36px"}} className="grid justify-center ">
             <div className="flex items-center justify-start">
-                <span style={{paddingBlock: "15px", paddingInline: "19px"}} className="icon-fi-rs-back bg-caak-titaniumwhite rounded-full"/>
+                <span onClick={() => history.goBack()} style={{paddingBlock: "15px", paddingInline: "19px"}} className="icon-fi-rs-back bg-caak-titaniumwhite rounded-full cursor-pointer"/>
                 <img alt="" style={{height: "52px", width: "52px", marginInlineStart: "20px", marginInlineEnd: "8px"}}  className="rounded-lg bg-caak-red"/>
                 <p className="text-24px font-medium text-caak-generalblack">Монгол өв уламжлал</p>
             </div>
-            <div style={{marginTop: "34px"}} className="flex">
-            <div style={{height: "170px"}} className="bg-white rounded-lg pr-c13">
+            <div style={{marginTop: "34px"}} className="2xl:flex xl:flex md:flex">
+                <div style={{height: "170px"}} className="bg-white rounded-lg pr-c13">
                             {group_data.map((list) => (
                                 <div
                                     className={`flex items-center cursor-pointer py-a1 rounded-lg ${list.title === selected ? "text-caak-primary" : "text-caak-generalblack"}`}
@@ -37,7 +39,7 @@ export default function Infromation() {
                                     <p className="text-17px font-medium">{list.title}</p>
                                 </div>
                             ))}
-                        </div>
+                </div>
                 <div style={{ width: "608px", marginInlineStart: "19px"}} className="bg-white rounded-lg">
                     <DropTest
                         className="relative w-full"
