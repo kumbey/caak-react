a3211ce506ac3bb62cf61cf1a4ad08cb67fb0eaf
weahforsage <sanjasapple@gmail.com>
2021-10-07 11:36:16 +0800

    remove: overlay provider

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import "./App.css";
 import PageSwitch from "./components/extra/PageSwitch";
-import { OverlayProvider } from "./context/useOverlay";
 
 function App() {
   return (
     <Router>
-      <OverlayProvider>
-        <PageSwitch />
-      </OverlayProvider>
+      <PageSwitch />
     </Router>
   );
 }
