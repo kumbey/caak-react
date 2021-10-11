86894a411c56fc202ff3acd5ec1adf05c5d3d306
weahforsage <sanjasapple@gmail.com>
2021-10-11 17:23:35 +0800

    add: post view X button

--- src/pages/Blog/ViewPost/index.js
+++ src/pages/Blog/ViewPost/index.js
@@ -100,7 +100,12 @@ const ViewPost = () => {
           "relative backBlur max-w-full w-full flex justify-center items-center h-full sm:h-1/2 md:h-1/2 lg:h-full"
         }
       >
-        {/*<div className={"nonBlur"}>*/}
+        <span
+          onClick={() => history.goBack()}
+          className={
+            "absolute cursor-pointer icon-fi-rs-close text-16px text-white top-4 right-4 z-2"
+          }
+        />
         {activeIndex > 0 && (
           <span
             onClick={() => prevItem()}
@@ -213,21 +218,12 @@ const ViewPost = () => {
         }
       >
         <div>
-          <div className={"cursor-pointer absolute right-4 top-0 z-50"}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-6 h-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-              />
-            </svg>
+          <div
+            className={
+              "flex justify-center items-center absolute right-4 top-6 z-10"
+            }
+          >
+            <span className={"cursor-pointer icon-fi-rs-dots text-4px mr-2"} />
           </div>
           <div className={"relative flex flex-row px-7"}>
             <div className={"relative"}>
