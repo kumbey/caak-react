023be881777648aeb31e7f1d1b6b393b14c358a7
kumurbek.b <kumurbek.b@gmail.com>
2021-10-18 14:12:53 +0800

    Notification seen all

--- src/components/PendingPost/PendingPostMenu.js
+++ src/components/PendingPost/PendingPostMenu.js
@@ -1,14 +1,25 @@
-export default function PendingPostMenu({edit}) {
-    return (
-        <div className="bg-white">
-            <div style={{paddingInlineEnd: "21px"}} onClick={edit} className="flex hover:bg-caak-liquidnitrogen h-c25 items-center cursor-pointer">
-                <span className={"icon-fi-rs-edit ml-b4 mr-b2 text-16px"} />
-                <p className="text-14px text-caak-extraBlack font-roboto">Фостыг засах</p>
-            </div>
-            <div style={{paddingInlineEnd: "21px"}}  className="flex hover:bg-caak-liquidnitrogen h-c25 items-center cursor-pointer">
-                <span className={"icon-fi-rs-delete ml-b4 mr-b2 text-16px"} />
-                <p className="text-14px text-caak-extraBlack font-roboto">Фостыг устгах</p>
-            </div>
-        </div>
-    )
+export default function PendingPostMenu({ edit }) {
+  return (
+    <div className="bg-white">
+      <div
+        style={{ paddingInlineEnd: "21px" }}
+        onClick={edit}
+        className="hover:bg-caak-liquidnitrogen h-c25 flex items-center cursor-pointer"
+      >
+        <span className={"icon-fi-rs-edit ml-b4 mr-b2 text-16px"} />
+        <p className="text-14px text-caak-extraBlack font-roboto">
+          Постыг засах
+        </p>
+      </div>
+      <div
+        style={{ paddingInlineEnd: "21px" }}
+        className="hover:bg-caak-liquidnitrogen h-c25 flex items-center cursor-pointer"
+      >
+        <span className={"icon-fi-rs-delete ml-b4 mr-b2 text-16px"} />
+        <p className="text-14px text-caak-extraBlack font-roboto">
+          Постыг устгах
+        </p>
+      </div>
+    </div>
+  );
 }
