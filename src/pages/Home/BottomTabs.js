e5ec78c6d59d9ed40f9d1034b7bee09bc6199e7a
weahforsage <sanjasapple@gmail.com>
2021-10-25 16:34:45 +0800

    fix: bottom tabs routing

--- src/pages/Home/BottomTabs.js
+++ src/pages/Home/BottomTabs.js
@@ -21,24 +21,58 @@ export default function BottomTabs() {
       <span className="icon-fi-fi-sp-hamburger-menu text-caak-blue text-24px px-b5 py-px-8 rounded-lg" />
       <span
         onClick={() =>
-          history.push({
-            pathname: "/post/add/new",
-            state: { background: location },
-          })
+          checkUser(user)
+            ? history.push({
+                pathname: "/post/add/new",
+                state: { background: location },
+              })
+            : history.push({
+                pathname: "/login",
+                state: { background: location },
+              })
         }
         className="icon-fi-rs-thick-add text-14px bg-caak-primary px-b5 py-px-8 text-white rounded-lg cursor-pointer"
       />
-      <span
-        onClick={() => setIsNotificationMenu(!isNotificationMenu)}
-        className={`${
-          isNotificationMenu
-            ? "icon-fi-rs-bookmark-f"
-            : "icon-fi-rs-notification"
-        } text-22px text-caak-blue px-b5 py-px-8 rounded-lg`}
-      />
+
+      <div
+        // ref={notificationRef}
+        onClick={() => {
+          checkUser(user)
+            ? setIsNotificationMenu((oldState) => !oldState)
+            : history.push({
+                pathname: "/login",
+                state: { background: location },
+              });
+        }}
+        className={"relative flex items-center cursor-pointer"}
+      >
+        <span
+          className={`${
+            isNotificationMenu
+              ? "icon-fi-rs-bookmark-f"
+              : "icon-fi-rs-notification"
+          } text-22px text-caak-blue px-b5 py-px-8 rounded-lg`}
+        />
+        {/*{parseInt(userTotal.unseen) > 0 ? (*/}
+        {/*  <span*/}
+        {/*    className={*/}
+        {/*      "absolute text-center top-1 -right-0.5 w-18px h-18px border-1 rounded-full border-white font-medium border border-white bg-caak-bleudefrance text-white text-12px"*/}
+        {/*    }*/}
+        {/*  >*/}
+        {/*    {userTotal.unseen > 9 ? "9+" : userTotal.unseen}*/}
+        {/*  </span>*/}
+        {/*) : null}*/}
+      </div>
+
       {checkUser(user) ? (
         <img
-          className={"rounded-full w-c3 h-c3"}
+          onClick={() =>
+            history.push({
+              pathname: `/user/${user.sysUser.id}/profile`,
+              state: { background: location },
+            })
+          }
+          className={"rounded-full w-c3 h-c3 cursor-pointer"}
           alt={user.sysUser.name}
           src={
             user.sysUser.pic
@@ -47,7 +81,15 @@ export default function BottomTabs() {
           }
         />
       ) : (
-        <span className="icon-fi-rs-profile text-24px text-caak-blue px-b5 py-px-8 rounded-lg" />
+        <span
+          onClick={() => {
+            history.push({
+              pathname: "/login",
+              state: { background: location },
+            });
+          }}
+          className="icon-fi-rs-profile text-24px text-caak-blue px-b5 py-px-8 rounded-lg"
+        />
       )}
     </div>,
     document.body
