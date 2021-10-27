968204875be77cc368e269b16bce9059490d2be7
weahforsage <sanjasapple@gmail.com>
2021-10-27 12:50:54 +0800

    fix: long group name text wrap

--- src/components/Sidebar/Suggest.js
+++ src/components/Sidebar/Suggest.js
@@ -1,29 +1,22 @@
-import { generateFileUrl } from "../../Utility/Util"
+import { generateFileUrl } from "../../Utility/Util";
 
-export default function Suggest({item,onClick}) {
-    return (
-        <div
-            onClick={onClick}
-            className={"flex flex-col cursor-pointer w-max"}
+export default function Suggest({ item, onClick }) {
+  return (
+    <div onClick={onClick} className={"flex flex-col cursor-pointer w-full"}>
+      <div className={"flex flex-row items-center p-1.5 my-px rounded-square"}>
+        <img
+          src={generateFileUrl(item.profile)}
+          className={"w-8 h-8 rounded-md object-cover mr-2"}
+          alt={""}
+        />
+        <span
+          className={
+            "text-caak-generalblack font-medium text-15px break-words"
+          }
         >
-            <div
-                className={
-                    "flex flex-row items-center p-1.5 my-px rounded-square"
-                }
-            >
-                <img
-                    src={generateFileUrl(item.profile)}
-                    className={"w-8 h-8 rounded-md object-cover mr-2"}
-                    alt={""}
-                />
-                <span
-                    className={
-                        "text-caak-generalblack font-medium text-15px"
-                    }
-                >
-                    {item.name}
-                </span>
-            </div>
-        </div>
-    )
+          {item.name}
+        </span>
+      </div>
+    </div>
+  );
 }
